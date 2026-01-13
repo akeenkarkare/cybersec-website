3595966ecd94780b17057b310d759d60461420fc
felix <felix.gerzon@stonybrook.edu>
2026-01-12 21:43:55 -0500

    better seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,16 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <head>
-        {/* todo: twitter and opengraph */}
         <title>SBU CyberSec Club | Stony Brook University</title>
+		<meta name="og:title" content='SBU CyberSec Club'/>
         <meta name="description" content="Official website of Stony Brook University Cybersecurity Club" />
+		<meta name='og:type' content='website'/>
+		<meta name='og:image' content='/favicon.ico'/>
         <meta name="keywords" content="Stony Brook,Cybersecurity,Club" />
         <meta name="author" content='Akeen' />
         <meta name="author" content='Felix' />
+		<meta name='og:url' content='https://www.sbucyber.org'/>
+		<meta name='og:description' content='This is the official website of Stony Brook University Cybersecurity Club. We provide an educational space for anyone interested in cybersecurity, hosting weekly meetings during the semester.'/>
       </head>
 	  	<body className="min-h-screen overflow-x-hidden">
         <PageStructure>
