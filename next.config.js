0852ad9a84f4e797135c3852a6b49d34bc20080a
felix <felix.gerzon@stonybrook.edu>
2026-02-12 21:44:21 -0500

    cybersecure headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	// async headers() {
+	// 	return [{
+	// 		source: '/(.*)',
+	// 		headers: [{
+	// 			key: 'Content-Security-Policy',
+	// 			value: 'default-src \'none\'; script-src \'self\'; style-src \'self\' fonts.googleapis.com \'unsafe-inline\'; img-src \'self\'; frame-ancestors \'none\'; font-src \'self\' fonts.gstatic.com fonts.googleapis.com; connect-src \'self\'; upgrade-insecure-requests;'
+	// 		}]
+	// 	}]
+	// },
+	poweredByHeader: false
+}
 
 module.exports = nextConfig
