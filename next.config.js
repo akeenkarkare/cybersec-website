9cd70a2e7750c272e1b624897538623efb928bb4
felix <felix.gerzon@stonybrook.edu>
2026-02-12 22:47:30 -0500

    nonces didn't work

--- next.config.js
+++ next.config.js
@@ -1,14 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// async headers() {
-	// 	return [{
-	// 		source: '/(.*)',
-	// 		headers: [{
-	// 			key: 'Content-Security-Policy',
-	// 			value: 'default-src \'none\'; script-src \'self\'; style-src \'self\' fonts.googleapis.com \'unsafe-inline\'; img-src \'self\'; frame-ancestors \'none\'; font-src \'self\' fonts.gstatic.com fonts.googleapis.com; connect-src \'self\'; upgrade-insecure-requests;'
-	// 		}]
-	// 	}]
-	// },
+	async headers() {
+  		// const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+		return ([{
+			source: '/(.*)',
+			headers: [{
+				key: 'X-XSS-Protection',
+				value: '1; mode=block'
+			},{
+				key: 'Referrer-Policy',
+				value: 'strict-origin-when-cross-origin'
+			},
+			{
+				key: 'content-security-policy',
+				// value: `default-src 'none'; script-src 'self' 'nonce-${nonce}' 'strict-dynamic'; style-src 'self' 'nonce-${nonce}' fonts.googleapis.com 'unsafe-inline'; img-src 'self'; frame-ancestors 'none'; font-src 'self' fonts.gstatic.com fonts.googleapis.com; connect-src 'self'; upgrade-insecure-requests;`
+				value: `default-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com 'unsafe-inline'; img-src 'self'; frame-ancestors 'none'; font-src 'self' fonts.gstatic.com fonts.googleapis.com; connect-src 'self'; upgrade-insecure-requests;`
+			},//{
+			// 	key: 'x-nonce',
+			// 	value: nonce
+			// }
+			]
+		}])
+	},
 	poweredByHeader: false
 }
 
